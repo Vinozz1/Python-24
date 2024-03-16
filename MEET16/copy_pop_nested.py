13ccfb73550b3519d7baed007a503f3887d91f89
Vinozz1 <jonathanalvino14@gmail.com>
2024-03-16 10:25:37 +0700

    testt

--- MEET16/copy_pop_nested.py
+++ MEET16/copy_pop_nested.py
@@ -2,60 +2,64 @@ dataDict = {
     "plm": "Palembang",
     "em": "Muara Enim",
     "pr": "Prabumulih",
-    "llg": "LubukLinggau",
+    "llg": "Lubuk Linggau"
 }
-data_copy = dataDict.copy()
+data_copy = dataDict
 dataDict["plm"] = "PALEMBANG"
 print(f"DataDict = {dataDict}")
 print(f"data_copy = {data_copy}")
-
 data_copy2 = dataDict.copy()
-dataDict["llg"] = "LUBUKLINGGAU"
+dataDict["llg"] = "LUBUK LINGGAU"
 print(f"DataDict = {dataDict}")
 print(f"data_copy2 = {data_copy2}")
 
 print("-" * 15, "POP")
 data_pop = dataDict.pop("plm")
-print(f"data_pop = {data_pop}")
+print(f"Data Pop = {data_pop}")
 print(f"dataDict = {dataDict}")
 
-print("-" * 15, "PopItem")
-data_popItem = dataDict.popitem()  # data menjadi tuple
-print(f"data_popItem = {data_popItem}")
+print("-" * 15, "Popitems")
+data_popitem = dataDict.popitem()  # Data menjadi tuple
+print(f"data_popItem = {data_popitem}")
 print(f"dataDict = {dataDict}")
 
 print("-" * 15, "NESTED DICTIONARY")
 dataNest = {
-    "mobill": {
-        "merk": "rush",
+    "mobil1": {
+        "merk": "push",
         "color": "red",
         "year": 2022
     },
     "mobil2": {
-        "merk": "avanza",
+        "merk": "toyota",
         "color": "putih",
         "year": 2020
     }
 }
-print(f"Merk Mobil1 = {dataNest['mobill']['merk']}")
-print(f"Merk Mobil2 = {dataNest.get('mobil2').get('merk')}")
-print(f"Color Mobil2 = {dataNest['mobil2']['color']}")
+print(f"Merk Mobil1 = {dataNest['mobil1']['merk']}")
+print(f"Merk Mobil2 = {dataNest['mobil2']['merk']}")
+print(f"color Mobil2 = {dataNest['mobil2']['color']}")
 
 print("-" * 15, "DEEPCOPY")
-from copy import deepcopy
+from copy import deepcopy 
 dataNest_copy = deepcopy(dataNest)
-dataNest["mobil2"]["merk"] = "Xenia"
-print( f" dataNest = { dataNest}")
-print( f" dataNest_copy= { dataNest_copy}")
+dataNest["mobil2"]["merk"] = "xenia"
+print(f"dataNest= {dataNest}")
+print(f"dataNest_copy= {dataNest_copy}")
 
-print("-"*15, "COMPREHENSION")
+print("-" * 15, "COMPREHENSION")
 data = "ABC"
-data1 = { i for i in data } # ini jdi data set
-print( f" data1 = { data1 }, type = { type ( data1 ) } ")
-data1 =  { i : i for i in data }
-print( f" data1 = { data1 }, type = { type ( data1) }")
-print("-"*15, "2. COMPREHENSION")
-data1 = [ "x", "y", "z"]
-data2 = [ 7, 8, 9 ]
-dataFinal = { k : v**3 for k,v in zip( data1, data2)}
-print( f"Data final = {dataFinal}")
+data1 = (i for i in data)  # ini jadi data set
+print(f"data1 = {data1}, type = {type(data1)} ")
+data1 = {i: i*3 for i in data}
+print(f"data1 = {data1}, type = {type(data1)} ")
+print("-" * 15, "2.COMPREHENSION")
+data1 = ["x", "y", "z"]
+data2 = [7, 8, 9]
+dataFinal = {k: v**3 for k, v in zip(data1, data2)}
+print(f"Data Final = {dataFinal}")
+
+print("-" * 15, "3.COMPREHENSION")
+data = {"ana": 85, "ibnu": 90, "indah": 100, "bunga": 86}
+data = {k: v for k, v in data.items() if v > 85}
+print(f"Data = {data}")
